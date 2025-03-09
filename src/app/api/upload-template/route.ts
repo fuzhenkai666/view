cf60c9b2b340aa5839ed5335917077929cc5f5e3
fuzhenkai666 <fzklouis@qq.com>
2025-03-09 16:08:39 -0700

    s

--- src/app/api/upload-template/route.ts
+++ src/app/api/upload-template/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import { writeFile } from 'fs/promises';
 import fs from 'fs/promises';
 import path from 'path';
-import os from 'os';
 
 export async function POST(request: Request): Promise<NextResponse> {
     try {
